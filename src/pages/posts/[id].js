504fc50749f93dfef6b25fb8463cd43113b7493b
Isaac McAuley <mcauleyisaac@gmail.com>
2023-01-25 22:34:20 -0500

    basics

---
+++ src/pages/posts/[id].js
@@ -0,0 +1,44 @@
+export default function Post({ post }) {
+    return (<div>
+        {post.data}
+    </div>)
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+    // params contains the post `id`.
+    // If the route is like /posts/1, then params.id is 1
+    // const res = await fetch(`https://.../posts/${params.id}`)
+    // const post = await res.json()
+    const post = {
+        data: "test"
+    }
+
+    // Pass post data to the page via props
+    return { props: { post } }
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+    // Call an external API endpoint to get posts
+    // const res = await fetch('https://.../posts')
+    // const posts = await res.json()
+
+    // Get the paths we want to pre-render based on posts
+    // const paths = posts.map((post) => ({
+    //     params: { id: post.id },
+    // }))
+
+    const paths = [
+        {
+            params: { id: "1" }
+        },
+        {
+            params: { id: "2" }
+        }
+    ]
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+}
